4e5d47bde5f7ebb59d3c0448c06725dc7afc4bb7
Sebastian Echeverria <secheverriamartinez@csumb.edu>
2025-02-26 11:38:12 -0800

    /user changed
    Changing URL param showcases different user objects

--- demo/src/main/java/com/example/demo/UserController.java
+++ demo/src/main/java/com/example/demo/UserController.java
@@ -1,25 +1,36 @@
 package com.example.demo;
 
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 // import org.springframework.boot.SpringApplication;
 // import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 // import com.example.demo.User;
 
 @RestController
 public class UserController {
+
+    private final AtomicInteger counter = new AtomicInteger();
     
     @GetMapping("/")
     public String index() {
         return "THIS IS THE ROOT ROUTE";
-    } 
+    }
 
     @GetMapping("/user")
-    public User showUser(){
-        User user = new User(1, "Sebastian", "123456");
+    public User showUser(@RequestParam(value = "username", defaultValue = "Sebastian") String username, @RequestParam(value = "password", defaultValue = "123456") String password){
+        User user = new User(counter.incrementAndGet(), username, password); 
         return user;
     }
+
+    @PostMapping("/addUser")
+    public User addUser(User user){
+        return user;
+    }
+
+    
 }
