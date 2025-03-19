befb3de13d84976bc98fbe3c4799f9a1fd40fd42
cbravo20 <cbravo-perez@csumb.edu>
2025-03-18 21:07:26 -0700

    removing / endpoint

--- src/main/java/com/example/demo/Controllers/Controller.java
+++ src/main/java/com/example/demo/Controllers/Controller.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    
+    /*
     @GetMapping("/")
     public String index() {
         return "Hello this is the / endpoint.";
     }
-
+*/
 
 
     @GetMapping("/greeting")
