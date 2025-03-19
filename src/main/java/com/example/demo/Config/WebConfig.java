2fc24b6b44a00bc46794e5be4be48b32e78aba23
cbravo20 <cbravo-perez@csumb.edu>
2025-03-18 20:17:10 -0700

    removing / endoint
    removing / endpoint to try and get front end on heroku

---
+++ src/main/java/com/example/demo/Config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true);
+    }
+
+    @Controller
+    public static class FallbackController {
+        @GetMapping(value = "/**/{path:[^.]*}")
+        public String forward() {
+            return "forward:/index.html";
+        }
+    }
+}
